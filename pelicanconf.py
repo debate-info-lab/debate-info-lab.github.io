c34b765c745ff41c2d39147643e3077bf0db4580
mugwort_rc <mugwort.rc@gmail.com>
2016-05-16 01:38:30 +0900

    Initial commit.

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'mugwort_rc'
+SITENAME = 'Debate Information Laboratory'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  None
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/debate-information-laboratory'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+SLUGIFY_SOURCE = 'basename'
+THEME = './pelican-bootstrap3'
+
