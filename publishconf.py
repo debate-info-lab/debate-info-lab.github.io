fb384f83036dc931b2ac018cd12156b7fefb0bb2
mugwort_rc <mugwort.rc@gmail.com>
2017-01-31 17:53:26 +0900

    Update hostname

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://info-labs.github.io'
+SITEURL = 'https://debate.info-labs.jp'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
