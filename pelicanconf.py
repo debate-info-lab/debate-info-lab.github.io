246db4966572db77c09d69f69cdd0f413529549f
mugwort_rc <mugwort.rc@gmail.com>
2017-02-03 23:12:53 +0900

    Update favicons

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,11 +30,83 @@ STATIC_PATHS = [
 ]
 
 EXTRA_PATH_METADATA = {
-    'static/CNAME': {
-        'path': 'CNAME',
+    'static/android-icon-36x36.png': {
+        'path': 'android-icon-36x36.png',
+    },
+    'static/android-icon-48x48.png': {
+        'path': 'android-icon-48x48.png',
+    },
+    'static/android-icon-72x72.png': {
+        'path': 'android-icon-72x72.png',
+    },
+    'static/android-icon-96x96.png': {
+        'path': 'android-icon-96x96.png',
+    },
+    'static/android-icon-144x144.png': {
+        'path': 'android-icon-144x144.png',
+    },
+    'static/android-icon-192x192.png': {
+        'path': 'android-icon-192x192.png',
+    },
+    'static/apple-icon.png': {
+        'path': 'apple-icon.png',
+    },
+    'static/apple-icon-57x57.png': {
+        'path': 'apple-icon-57x57.png',
+    },
+    'static/apple-icon-72x72.png': {
+        'path': 'apple-icon-72x72.png',
+    },
+    'static/apple-icon-76x76.png': {
+        'path': 'apple-icon-76x76.png',
+    },
+    'static/apple-icon-114x114.png': {
+        'path': 'apple-icon-114x114.png',
+    },
+    'static/apple-icon-120x120.png': {
+        'path': 'apple-icon-120x120.png',
+    },
+    'static/apple-icon-144x144.png': {
+        'path': 'apple-icon-144x144.png',
+    },
+    'static/apple-icon-152x152.png': {
+        'path': 'apple-icon-192x192.png',
+    },
+    'static/apple-icon-180x180.png': {
+        'path': 'apple-icon-180x180.png',
+    },
+    'static/apple-icon-precomposed.png': {
+        'path': 'apple-icon-precomposed.png',
+    },
+    'static/browserconfig.xml': {
+        'path': 'broserconfig.xml',
     },
     'static/favicon.ico': {
         'path': 'favicon.ico',
+    },
+    'static/favicon-16x16.ico': {
+        'path': 'favicon16x16.ico',
+    },
+    'static/favicon32x32.ico': {
+        'path': 'favicon32x32.ico',
+    },
+    'static/favicon96x96.ico': {
+        'path': 'favicon96x96.ico',
+    },
+    'static/manifest.json': {
+        'path': 'manifest.json',
+    },
+    'static/ms-icon-70x70.png': {
+        'path': 'ms-icon-70x70.png',
+    },
+    'static/ms-icon-144x144.png': {
+        'path': 'ms-icon-144x144.png',
+    },
+    'static/ms-icon-150x150.png': {
+        'path': 'ms-icon-150x150.png',
+    },
+    'static/ms-icon-310x310.png': {
+        'path': 'ms-icon-310x310.png',
     },
 }
 
